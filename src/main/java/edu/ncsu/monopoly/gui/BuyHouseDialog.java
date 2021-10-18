c904a98c1073b7b70990f70fb1128bedf8b0f1ec
Tserenlkham <okono2503@gmail.com>
2021-10-19 00:33:36 +0200

    BuyHouseDialog

--- src/main/java/edu/ncsu/monopoly/gui/BuyHouseDialog.java
+++ src/main/java/edu/ncsu/monopoly/gui/BuyHouseDialog.java
@@ -15,7 +15,7 @@ import edu.ncsu.monopoly.Player;
 
 
 public class BuyHouseDialog extends JDialog {
-	private JComboBox cboMonopoly; 
+	private JComboBox <String> cboMonopoly; 
 	private JComboBox cboNumber;
 
 	private Player player;
